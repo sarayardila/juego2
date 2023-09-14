eb883cc280f8aa3776edb2d6e833c87152585b82
saray ardila <132412051+sarayardila@users.noreply.github.com>
2023-09-14 18:50:56 -0500

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,9 @@
+function back() {
+window.location = "activity_1.html";
+}
+
+function getScore()
+{
+	score = localStorage.getItem("score");	
+	document.getElementById("update").innerHTML = "<h1>Puntaje: " + score +"</h1>";
+}
